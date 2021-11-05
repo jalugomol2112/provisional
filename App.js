b2ac8620b7b59d9b280a43859b883986b199a731
jalugomol2112 <90425998+jalugomol2112@users.noreply.github.com>
2021-11-05 12:32:38 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Buscador from './componentes/Buscador'
+import Resultado from "./componentes/Resultado";
+class App extends Component {
+
+  state = {
+    termino : '',
+    imagenes : [],
+    pagina : ''
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'end');
+  }
+
+  paginaAnterior = () => {
+    let pagina = this.state.pagina;
+
+    if(pagina === 1) return null;
+
+    pagina -= 1;
+
+    this.setState({
+      pagina
+    },() => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    //console.log(pagina);
+
+  }
+  paginaSiguiente = () => {
+    let pagina = this.state.pagina;
+
+    pagina += 1;
+
+    this.setState({
+      pagina
+    },() => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    //console.log(pagina);
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+
+    const url = `https://pixabay.com/api/?key=24051813-fdd4050fee8af0f7b6e2a2d71&q=${termino}&per_page=30&page=${pagina}`;
+
+    console.log(url);
+    fetch(url)
+     .then(respuesta => respuesta.json() )
+     .then(resultado => this.setState({ imagenes : resultado.hits}) )
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+      
+    }, () => {
+      this.consultarApi();
+    })
+  
+  }
+render() {
+  return (
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de productos</p>
+
+        <Buscador 
+          datosBusqueda={this.datosBusqueda}
+        />
+      
+      
+      </div>
+      <div className="row justify-content-center">
+         <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+      
+         />
+      </div>
+    </div>
+
+   );
+ }
+}
+
+
+export default App;
